47f08a28bb42af272b9f667207f43f86875f6932
blupxd <matija.stefanovic050@gmail.com>
2025-08-14 16:13:44 +0200

    male izmene oko responsive desgin-a

--- src/components/leaderboard/LeaderboardElement.tsx
+++ src/components/leaderboard/LeaderboardElement.tsx
@@ -8,11 +8,11 @@ interface LeaderboardElementProps {
 const LeaderboardElement = ({ number, name, time }: LeaderboardElementProps) => {
   return (
     <div className='flex w-full items-center px-4 py-2 gap-4 rounded-lg bg-tertiary shadow-[0_4px_0_0_#182235]'>
-      <span className='text-3xl font-bold'>#{number}</span>
-      <img src={dzoni} width={55} height={55} className='rounded-full border-2 border-white' alt='Ikonica'/>
+      <span className='text-2xl lg:text-3xl font-bold'>#{number}</span>
+      <img src={dzoni} width={55} height={55} className='rounded-full md:w-auto w-10 border-2 border-white' alt='Ikonica'/>
       <div className='flex flex-col'>
-        <span className='leading-tight text-xl font-bold'>{name}</span>
-        <span className='leading-tight font-light text-base'>{time}</span>
+        <span className='leading-tight md:text-lg lg:text-xl font-bold'>{name}</span>
+        <span className='leading-tight font-light text-xs md:text-sm lg:text-base'>{time}</span>
       </div>
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,10 +11,10 @@ const Home = () => {
   return startedGame ? (
     <Game setStartedGame={setStartedGame} gameMode={selectedMode} />
   ) : (
-    <main className="min-h-screen w-screen flex flex-col items-center justify-center">
+    <main className="m-4 min-h-screen flex flex-col items-center justify-center">
       <img src={logo} width={500} height={500} />
-      <div className="max-w-md flex flex-col gap-6">
-        <button onClick={() => setStartedGame(true)} className="bg-light-blue cursor-pointer px-20 py-6 text-2xl font-black rounded-xl shadow-[0_6px_0_0_#96E8FF] hover:shadow-[0_4px_0_0_rgb(135,206,250)] hover:translate-y-[2px] transition-all duration-150">
+      <div className="w-full lg:max-w-md flex flex-col gap-6">
+        <button onClick={() => setStartedGame(true)} className="bg-light-blue cursor-pointer px-20 py-4 lg:py-6 w-full lg:text-2xl font-black rounded-xl shadow-[0_6px_0_0_#96E8FF] hover:shadow-[0_4px_0_0_rgb(135,206,250)] hover:translate-y-[2px] transition-all duration-150">
           ZAPOČNI IGRU
         </button>
         <div className="grid grid-cols-3 bg-[#272C36] p-1 rounded-md gap-1">
@@ -24,7 +24,7 @@ const Home = () => {
               className={`px-2 font-bold hover:bg-baby-yellow transition-colors duration-300 ease-in-out cursor-pointer hover:text-white py-1 rounded-md ${
                 selectedMode === mode
                   ? "bg-baby-yellow"
-                  : "bg-gray-700 text-white/20"
+                  : "bg-tertiary text-white/20"
               }`}
               onClick={() => setSelectedMode(mode)}
             >
@@ -33,7 +33,7 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div className="flex gap-4 mt-4 flex-col w-full max-w-2xl">
+      <div className="flex gap-4 mt-4 flex-col w-full lg:max-w-2xl">
         <h2 className="text-lg font-bold">Leaderboard</h2>
         <div className="flex overflow-hidden relative flex-col gap-4">
           <div className="bg-gradient-to-t from-primary to-primary/0 absolute inset-0"/>
