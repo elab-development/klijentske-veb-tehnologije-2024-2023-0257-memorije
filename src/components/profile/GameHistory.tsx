9ab4845d0bf94cba489bd44246527b1481c0b7b3
Jovana Antic <anticjovana93@gmail.com>
2025-08-26 16:41:05 +0200

    Napravljen profil

--- src/components/profile/GameHistory.tsx
+++ src/components/profile/GameHistory.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-
+import joystick from "../../assets/joystick.svg";
 interface GameHistoryProps {
-    time:string;
-    accuracy:number; 
+  time: string;
+  accuracy: number;
+  played: Date;
+  mode: string;
 }
 
-const GameHistory = ({time,accuracy}:GameHistoryProps) => {
+const GameHistory = ({ time, accuracy, played, mode }: GameHistoryProps) => {
   return (
-    <div className='rounded-xl bg-light-gray flex'>
-        <div className='flex flex-col'>
-            <h3 className='font-bold text-xl'>VREME</h3>
-            <span className='text-xl'>{time}</span>
-        </div>
-        <div className='text-baby-yellow flex flex-col'>
-            <h3 className='font-bold text-xl'>POGOCI</h3>
-            <span className='text-xl'>{accuracy}</span>
-        </div>
+    <div className="rounded-xl bg-tertiary px-6 py-4 gap-10 flex w-full">
+      <img src={joystick} alt="dzojstik" width={55} height={55} />
+      <div className="flex flex-col">
+        <h3 className="font-bold text-lg lg:text-xl">
+          {played.toLocaleDateString("en-GB").replaceAll("/", ".")}.
+        </h3>
+        <span className="text-lg lg:text-xl">{time}</span>
+      </div>
+      <div className="text-baby-yellow mx-auto flex flex-col">
+        <h3 className="font-bold text-lg lg:text-xl">TEŽINA</h3>
+        <span className="text-lg lg:text-xl">{mode}</span>
+      </div>
+      <div className="text-baby-yellow flex flex-col">
+        <h3 className="font-bold text-lg lg:text-xl">POGOCI</h3>
+        <span className="text-lg lg:text-xl">{accuracy}%</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GameHistory
+export default GameHistory;
