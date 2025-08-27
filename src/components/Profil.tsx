5d10b800dcc15e12645ecd904391d6bfefb2246a
blupxd <matija.stefanovic050@gmail.com>
2025-08-27 21:38:25 +0200

    reseno

--- src/components/Profil.tsx
+++ src/components/Profil.tsx
@@ -1,18 +1,19 @@
 import { getCurrentUser } from "../lib/authStorage";
 import LogoutButton from "./auth/LogoutButton";
 import profilIkonica from "../assets/dzoni.png";
+import { Link } from "react-router-dom";
 
 export const Profil = () => {
   const user = getCurrentUser();
   return (
     <div className="lg:absolute lg:mt-0 mt-4 left-14 z-30 top-14 rounded-xl lg:border border-light-gray lg:bg-secondary px-6 py-4 flex lg:flex-row flex-col lg:items-start items-center gap-2 lg:gap-4">
-      <div className="relative overflow-hidden rounded-full border-5 lg:border-4 border-white aspect-square w-18 lg:w-16">
+      <Link to='/profile' className="relative overflow-hidden rounded-full border-5 lg:border-4 border-white aspect-square w-18 lg:w-16">
         <img
           src={profilIkonica}
           alt="Ikonica"
           className="object-cover w-full h-full"
         />
-      </div>
+      </Link>
       <div className="flex flex-col">
         <h3 className="text-2xl lg:text-xl font-bold">
           {user?.firstName} {user?.lastName}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,31 +1,108 @@
-import { useState } from "react";
-import VictoryModal from "../components/VictoryRoyale"; 
+import { getCurrentUser } from "../lib/authStorage";
+import dzoni from "../assets/dzoni.png";
+import GameHistory from "../components/profile/GameHistory";
+import type { Partija } from "../types/auth";
+import CustomButton from "../components/CustomButton";
+import { Undo2 } from "lucide-react";
 
 export default function Profile() {
-  const [open, setOpen] = useState(false);
-
+  const userData = getCurrentUser();
+  const formatSec = (totalSec: number) => {
+    const m = Math.floor(totalSec / 60);
+    const s = totalSec % 60;
+    return `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
+  };
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen">
-      {!open && (
-        <button
-          onClick={() => setOpen(true)}
-          className="px-6 py-3 bg-blue-500 text-white rounded-xl"
-        >
-          Završi igru (otvori modal)
-        </button>
-      )}
-
-      <VictoryModal
-        open={open}
-        time="00:32"
-        accuracy={92.3}
-        onReplay={() => {
-          setOpen(false);
-        }}
-        onHome={() => {
-          setOpen(false);
-        }}
-      />
-    </div>
+    <>
+      <CustomButton to="/">
+        <Undo2 size={32} />
+      </CustomButton>
+      <div className="p-14 relative flex flex-col items-center min-h-screen">
+        <div className="flex flex-col items-center gap-10">
+          <img
+            src={dzoni}
+            alt="Profilna ikonica"
+            width={300}
+            height={300}
+            className="rounded-full border-4 border-white"
+          />
+          <h1 className="font-bold text-3xl">
+            {userData?.firstName} {userData?.lastName}
+          </h1>
+        </div>
+        <div className="grid xl:grid-cols-2 mt-16 w-full gap-10 md:gap-4 lg:gap-10 xl:gap-40">
+          <div className="flex max-h-96 pr-2 overflow-auto flex-col gap-4 w-full items-center">
+            {userData?.games.map((game: Partija, idx: number) => (
+              <GameHistory
+                played={new Date(game.playedAt)}
+                mode={game.mode}
+                accuracy={game.accuracy}
+                key={idx}
+                time={formatSec(game.timeMs / 1000)}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="rounded-xl flex-col bg-tertiary p-4 flex items-center justify-center gap-4 w-full">
+              <span className="text-3xl lg:text-5xl xl:text-7xl font-black">
+                {userData?.games.length || 0}
+              </span>
+              <h2 className="font-black text-xl text-center text-[#989898]">
+                ODIGRANE IGRE
+              </h2>
+            </div>
+            <div className="rounded-xl flex-col bg-tertiary p-4 flex items-center justify-center gap-4 w-full">
+              <span className="text-3xl lg:text-5xl xl:text-7xl font-black">
+                {userData?.games.length
+                  ? formatSec(
+                      Math.floor(
+                        userData.games.reduce(
+                          (acc, game) => acc + game.timeMs,
+                          0
+                        ) /
+                          userData.games.length /
+                          1000
+                      )
+                    )
+                  : "00:00"}
+              </span>
+              <h2 className="font-black text-xl text-center text-[#989898]">
+                PROSEČNO VREME
+              </h2>
+            </div>
+            <div className="rounded-xl flex-col bg-tertiary p-4 flex items-center justify-center gap-4 w-full">
+              <span className="text-3xl lg:text-5xl xl:text-7xl font-black">
+                {userData?.games.length
+                  ? (
+                      userData.games.reduce(
+                        (acc, game) => acc + game.accuracy,
+                        0
+                      ) / userData.games.length
+                    ).toFixed(1)
+                  : "0"}
+                %
+              </span>
+              <h2 className="font-black text-xl text-center text-[#989898]">
+                PROCENAT POGODAKA
+              </h2>
+            </div>
+            <div className="rounded-xl flex-col bg-tertiary p-4 flex items-center justify-center gap-4 w-full">
+              <span className="text-3xl lg:text-5xl xl:text-7xl font-black">
+                {userData?.games.length
+                  ? formatSec(
+                      Math.min(
+                        ...userData.games.map((game) => game.timeMs / 1000)
+                      )
+                    )
+                  : "00:00"}
+              </span>
+              <h2 className="font-black text-xl text-center text-[#989898]">
+                NAJBOLJI REZULTAT
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
